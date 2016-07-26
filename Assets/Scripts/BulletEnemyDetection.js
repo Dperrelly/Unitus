47b5402db8d8f65cb82ed262178c3b4626733da4
Daniel Perrelly <danperrelly@gmail.com>
2016-07-26 12:42:23 -0400

    player death

--- Assets/Scripts/BulletEnemyDetection.js
+++ Assets/Scripts/BulletEnemyDetection.js
@@ -3,7 +3,7 @@
 var power : int = 3;
 
 function OnTriggerEnter2D(other : Collider2D) {
-    if(other.gameObject.tag !== "Player"){
+    if(other.gameObject.tag != "Player" && other.gameObject.tag != "Nono" ){
         var otherScript = other.gameObject.GetComponent.<EnemyHealth>();
         otherScript.takeDamage(power);
         Destroy(gameObject);
---
+++ Assets/ShipCollisionDetector.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+import UnityEngine.SceneManagement;
+
+public function die(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
+
+function OnTriggerEnter2D(other : Collider2D) {
+    if(other.gameObject.tag == "DestroysShip"){
+        die();
+    }
+}
