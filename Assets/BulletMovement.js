8395b5a2e91f1637cb9bac5fe3283bc93358788c
S. Scalice <sscalice92@gmail.com>
2016-07-26 04:43:15 -0700

    bullets

---
+++ Assets/BulletMovement.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+var speed : float = 2000;
+function Start () {
+    GetComponent.<Rigidbody>().AddForce(transform.forward*speed);
+}
+
+function Update () {
+
+}
---
+++ Assets/ShotManager.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var bulletPrefab:Transform;
+
+function Update () {
+    if(Input.GetButtonDown("Fire1"))
+    {
+        var bullet = Instantiate(bulletPrefab, GameObject.Find("projectileSpawnPoint").transform.position, Quaternion.identity);  
+    }
+}
